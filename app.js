8342a90dab8a12631b7a24991f79b00eed4bf746
Lucas Yepez <lucasyepez99@gmail.com>
2024-02-22 00:33:00 -0600

    set up game play

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 //make func to click on board square to place 'x' or 'o'
 //if spot is already taken don't let them take it
 
-//bitton to start and restart
+
 //display element that shows results of game at end
 
 let gameBoard = (function() {
@@ -16,43 +16,140 @@ let gameBoard = (function() {
         ['0', '0', '0']
     ];
 
-    let player = function() {
+
+    let gameBoardEle = document.querySelector('#gameBoard');
+
+    let player = function(name, type) {
         let score = 0;
+
+        return { score, type, name };
     }
+
+    let playerTurn = 'Player2';
+
     let displayController = function(board) {
-        let gameBoardEle = document.querySelector('#gameBoard');
-        let startBtn = document.createElement('button');
-        let startGame = () => {
+        board = [
+            ['0', '0', '0'],
+            ['0', '0', '0'],
+            ['0', '0', '0']
+        ];
+        let rowsToRemove = gameBoardEle.querySelectorAll(".rowContainer");
+        rowsToRemove.forEach((row) => row.remove());
 
-        }
-        let restartGame = () => {
-            let board = [
-                ['0', '0', '0'],
-                ['0', '0', '0'],
-                ['0', '0', '0']
-            ];
 
-            displayController(board);
-            startGame();
-        }
-        startBtn.addEventListener("click", startGame);
-        endBtn.addEventListener("click", restartGame);
 
-        let restartBtn = document.createElement('button');
-        board.forEach((row) => {
+        board.forEach((row, rowIndex) => {
             let rowEle = document.createElement('div');
             rowEle.classList.add("rowContainer");
-            row.forEach((piece) => {
+            row.forEach((piece, colIndex) => {
                 let pieceEle = document.createElement('div');
+                let updateVal = (row, col) => {
+                    console.log("update val", row, col)
+                    if (playerTurn == 'Player1') {
+                        console.log('player1.type', player1.type)
+                        pieceEle.textContent = player1.type;
+                        board[row][col] = player1.type;
+                        playerTurn = 'Player2';
+                    } else {
+                        pieceEle.textContent = player2.type;
+                        board[row][col] = player2.type;
+                        playerTurn = 'Player1';
+                    }
+                    let checkScore = () => {
+                        /*
+                          0,0  0,1  0,2
+                          1,0  1,1  1,2
+                          2,0  2,1  2,2
+
+
+                         1. three in a col:
+
+
+                          for each row 0,1,2
+
+                          if val row index 0 col index 0
+                          if val at row index 1 col index 0
+                          if val at row index 2 col index 0
+
+                          is x or o
+
+                          if val row index 0 col index 1
+                          if val at row index 1 col index 1
+                          if val at row index 2 col index 1
+
+                          is x or o
+
+                          if val row index 0 col index 2
+                          if val at row index 1 col index 2
+                          if val at row index 2 col index 2
+
+                          is x or 0
+
+                         2. three diagonal
+
+                            for each row 0,1,2
+                            if val at row index 0 and col index 0 is x or 0
+                            if row at row index 1 and col index 1 is x or 0
+
+                            if val at row index 2 and col index  2 is x or o
+
+                          3. three diagonal reverse
+                            for each row 0,1,2
+                            if val at row index 2, col index 2 is x or 0
+                            if val at row index  1, col index 1 is x or o
+                            if val at row index 0,col index 0 is x or o
+
+                          4. three in a row
+                            for each row
+                              if all three cols equal x or o
+
+                        */
+                        console.log("board", board);
+                        board.forEach((row) => {
+                            row.forEach((col) => {
+                                let val = board[row][col];
+                                console.log("val ", val)
+                            })
+                        })
+                    }
+                    checkScore(board);
+                }
+
                 pieceEle.textContent = piece;
                 pieceEle.classList.add("pieceEle");
+                pieceEle.addEventListener('click', function() {
+                    updateVal(rowIndex, colIndex)
+                });
+
                 rowEle.appendChild(pieceEle);
             })
             gameBoardEle.appendChild(rowEle);
         })
     }
-    return { displayController };
+    let startGame = () => {
+        console.log("start game");
+        displayController(board);
+    }
+    let restartGame = () => {
+
+        displayController(board);
+    }
+    let startBtn = document.createElement('button');
+    let restartBtn = document.createElement('button');
+    startBtn.addEventListener("click", startGame);
+    restartBtn.addEventListener("click", restartGame);
+    startBtn.textContent = "Start";
+    restartBtn.textContent = "Start Over";
+    gameBoardEle.appendChild(startBtn);
+    gameBoardEle.appendChild(restartBtn);
+    let player1 = player("1", "X");
+    let player2 = player("2", "O");
+
+    return { displayController, board, player };
 
 })();
+let turn = gameBoard.playerTurn;
+let board = gameBoard.board;
 
-gameBoard.displayController();
+
+//gameBoard.displayController(board);
