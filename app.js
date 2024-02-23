c5bfec587d8fd65dd650aaf515062e1435a187d4
Lucas Yepez <lucasyepez99@gmail.com>
2024-02-23 00:29:53 -0600

    updated

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ const gameBoard = (function() {
     let gameBoardEle = document.querySelector('#gameBoard');
 
     let player = function(name, type) {
+        let score = 0;
+        let addPoint = () => score++;
 
-
-        return { score, type, name };
+        return { score, type, name, addPoint };
     }
+    let player1 = player("1", "X");
+    let player2 = player("2", "O");
 
     let playerTurn = 'Player1';
-    let score = 0;
+
     let displayController = function(board) {
         board = [
             ['0', '0', '0'],
@@ -31,8 +34,10 @@ const gameBoard = (function() {
                 pieceEle.classList.add("pieceEle");
                 pieceEle.addEventListener('click', function() {
                     updateVal(rowIndex, colIndex)
+                    console.log("update val ", updateVal(rowIndex, colIndex))
                 });
                 let updateVal = (row, col) => {
+                    console.log("playerTurn", playerTurn)
                     if (playerTurn == 'Player1') {
                         console.log('player1.type', player1.type)
                         pieceEle.textContent = player1.type;
@@ -43,7 +48,7 @@ const gameBoard = (function() {
                         board[row][col] = player2.type;
                         playerTurn = 'Player1';
                     }
-                    let checkScore = (marker) => {
+                    let checkScore = (marker, player) => {
                         let three_in_column = (marker) => {
                             let row1 = board[0];
                             let row2 = board[1];
@@ -53,6 +58,7 @@ const gameBoard = (function() {
                                 let col2 = row2[i];
                                 let col3 = row3[i];
                                 if (col1 === marker && col2 === marker && col3 === marker) {
+                                    player.addPoint();
                                     return true;
                                 }
 
@@ -64,19 +70,28 @@ const gameBoard = (function() {
                             let row1 = board[0];
                             let row2 = board[1];
                             let row3 = board[2];
+                            let result = false;
+
 
                             for (let i = 0; i < 2; i++) {
                                 let row = board[i];
+                                let count = 0;
                                 row.forEach((piece) => {
+
                                     if (piece === marker) {
                                         count++;
                                     }
-                                    if (count === 3) {
-                                        return true;
-                                    }
+
                                 })
+                                if (count === 2) {
+                                    console.log("count === 2 ", count === 2)
+                                    result = true;
+                                    player.addPoint();
+
+                                }
                             }
-                            return false;
+                            console.log("result", result)
+                            return result;
                         }
 
                         let diagonalScore = (marker) => {
@@ -84,6 +99,7 @@ const gameBoard = (function() {
                             let row2 = board[1];
                             let row3 = board[2];
                             if (row1[0] === marker && row2[1] === marker && row3[2] === marker) {
+                                player.addPoint();
                                 return true;
                             }
                             return false;
@@ -94,13 +110,31 @@ const gameBoard = (function() {
                             let row2 = board[1];
                             let row3 = board[2];
                             if (row1[2] === marker && row2[1] === marker && row3[0] === marker) {
+                                player.addPoint();
                                 return true;
                             }
                             return false;
                         }
+
+                        if (three_in_column(marker) || three_in_row(marker) || diagonalScore(marker) || reverseDiagonal(marker)) {
+                            return true;
+                        } else {
+                            return false;
+                        }
                     }
                     console.log("checking board ", board);
-                    checkScore(board);
+                    let player1finalScore = checkScore(player1.type, player1);
+                    let player2finalScore = checkScore(player2.type, player2);
+                    console.log("player1finalScore", player1finalScore)
+                    console.log("player2finalScore", player2finalScore)
+                    if (player1finalScore) {
+
+                        return `${player1.name} is the winner!`;
+                    } else if (player2finalScore) {
+                        return `${player2.name} is the winner!`;
+                    } else if (player1.score === player2.score) {
+                        return "It's a tie!";
+                    }
                 }
                 rowEle.appendChild(pieceEle);
                 gameBoardEle.appendChild(rowEle);
@@ -125,8 +159,7 @@ const gameBoard = (function() {
     restartBtn.textContent = "Start Over";
     gameBoardEle.appendChild(startBtn);
     gameBoardEle.appendChild(restartBtn);
-    let player1 = player("1", "X");
-    let player2 = player("2", "O");
+
 
     return { displayController, board, player };
 })();
